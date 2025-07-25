70021d3bced42c33215746a744d83fe1abf0ffcf
anngy02 <hernandezangelique682@gmail.com>
2025-07-24 22:07:27 -0600

    ProyectoM3

--- Longdeunafrase.py
+++ Longdeunafrase.py
@@ -8,6 +8,7 @@ elif longitud < 4:
 else:
     print(f"Sobran letras. Tiene {longitud} letras.")
 
+
 palabra2= input ("ingrese una palabra: ")
 longitud= len(palabra2)
 
@@ -18,9 +19,9 @@ elif longitud < 4:
 else:
     print(f"Sobran letras. Tiene {longitud} letras.")
 
+
 palabra3= input ("ingrese una palabra: ")
 longitud= len(palabra3)
-
 
 if 4 <= longitud <= 8:
     print("La palabra es correcta.")
@@ -28,5 +29,3 @@ elif longitud < 4:
     print(f"Hacen falta letras. Solo tiene {longitud} letras.")
 else:
     print(f"Sobran letras. Tiene {longitud} letras.")
-
-   
